8a401ed9be55f3138f95612e982dd9efa2c8f7a4
pranay-vohra <pranayvohra77@gmail.com>
2024-11-10 18:01:02 +0530

    Initial Commit

---
+++ RPS Training/RPS_TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPS_TrainingApp.swift
+//  RPS Training
+//
+//  Created by pranay vohra on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RPS_TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RPS TrainingTests/RPS_TrainingTests.swift
@@ -0,0 +1,17 @@
+//
+//  RPS_TrainingTests.swift
+//  RPS TrainingTests
+//
+//  Created by pranay vohra on 10/11/24.
+//
+
+import Testing
+@testable import RPS_Training
+
+struct RPS_TrainingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
